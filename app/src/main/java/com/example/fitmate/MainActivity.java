4d56be0fb797c9c2ed3dbe1adc92b05be18b2dac
Saurabh Kumar Suryan <42460131+sksuryan@users.noreply.github.com>
2019-10-14 14:25:48 +0530

    fixed project issues

--- app/src/main/java/com/example/fitmate/MainActivity.java
+++ app/src/main/java/com/example/fitmate/MainActivity.java
@@ -1,14 +1,11 @@
 package com.example.fitmate;
 
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
