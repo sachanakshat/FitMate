766c5895c9aee73c43379dd2c5f08910bfb54284
Saurabh Kumar Suryan <42460131+sksuryan@users.noreply.github.com>
2019-10-15 14:01:43 +0530

    GPS Location based AQI

--- app/src/main/java/com/example/fitmate/MainActivity.kt
+++ app/src/main/java/com/example/fitmate/MainActivity.kt
@@ -1,37 +1,37 @@
 package com.example.fitmate
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.WindowManager
-import android.widget.TextView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.aqi_data.*
+import org.json.JSONException
 import org.json.JSONObject
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity(){
 
-    var requestQueue: RequestQueue? = null
+    var volleyReq: RequestQueue? = null
     var coordinates:String? = "28.605280;77.105402"
-    var gpsLocation: Location? = null
-    var networkLocation: Location? = null
+    var string:String? = null
 
     private var permission = arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION)
 
-    var url:String? = null
+    var url = "https://api.waqi.info/feed/geo:$coordinates/?token=8b13f1ca1e0c3d60b7cf452c526419c63cbc9e71"
 
     companion object {
         private const val PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 100
@@ -43,17 +43,27 @@ class MainActivity : AppCompatActivity(){
         w.addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission()
+        }
+        volleyReq = Volley.newRequestQueue(this)
+        aqi_data.isClickable = true
+        aqi_data.setOnClickListener{
+            getLocation()
+        }
 
     }
 
+    @SuppressLint("MissingPermission")
     fun getLocation(){
         var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        var locationListener = object : LocationListener{
+        var locationListener = object : LocationListener {
             override fun onLocationChanged(location: Location?) {
-                if(location != null) {
-                    gpsLocation = location
-                    coordinates = "${location.longitude};${location.latitude}"
-                    url = "https://api.waqi.info/feed/geo:"+coordinates+"/?token=8b13f1ca1e0c3d60b7cf452c526419c63cbc9e71";
+                if (location != null) {
+                    coordinates = "${location!!.longitude};${location!!.latitude}"
+                    url = "https://api.waqi.info/feed/geo:" + coordinates + "/?token=8b13f1ca1e0c3d60b7cf452c526419c63cbc9e71";
+                    getJSONObject(url)
                 }
             }
 
@@ -64,11 +74,7 @@ class MainActivity : AppCompatActivity(){
             override fun onProviderDisabled(p0: String?) {}
         }
 
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermission()
-        }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0f,locationListener)
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 10f, locationListener)
     }
 
     fun requestPermission() {
@@ -79,24 +85,25 @@ class MainActivity : AppCompatActivity(){
         return
     }
 
-    fun getData(){
-        var aqiReceiver = JsonObjectRequest(Request.Method.GET,url,Response.Listener {
+    private fun getJSONObject(url: String){
+        var req = JsonObjectRequest(Request.Method.GET,url,Response.Listener {
             response: JSONObject ->
             try {
                 var data = response.getJSONObject("data")
-                var iaqi = data.getJSONObject("iaqi")
-                var pm25 = iaqi.getJSONObject("pm25")
-                var value = pm25.getInt("v")
-                var aqiData = findViewById<TextView>(R.id.aqi_data)
-                aqiData.text = "PM2.5: $value"
-            }catch (e: Exception){
+                data = data.getJSONObject("iaqi")
+                //var dom = data.get()
+                data = data.getJSONObject("pm25")
+                var value = data.getInt("v")
+                string = "PM2.5: $value"
+                levels.text = string
+                Log.d("Response: ", response.toString())
+                //Log.d("PM2.5: ",value.toString())
+
+            } catch (e: JSONException){
                 e.printStackTrace()
             }
-
-        }, Response.ErrorListener {
-
+        },Response.ErrorListener {
         })
-
-        requestQueue!!.add(aqiReceiver)
+        volleyReq!!.add(req)
     }
 }
