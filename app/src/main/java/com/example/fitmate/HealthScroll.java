78cb85a8ffa0c8835773dd8a56b8136aec30fa31
Akshat Sachan <TheRedHatCoder@localhost.localdomain>
2019-10-15 10:14:42 +0530

    Recycler

---
+++ app/src/main/java/com/example/fitmate/HealthScroll.java
@@ -0,0 +1,14 @@
+package com.example.fitmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HealthScroll extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_health_scroll);
+    }
+}
